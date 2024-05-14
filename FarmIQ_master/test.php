f5f20775ca10902ddf8dcfd6cf8770f35fce7b73
LukaRiz <92185057+LukaRiz@users.noreply.github.com>
2024-05-14 13:57:22 +0200

    Delete FarmIQ_master directory

--- FarmIQ_master/test.php
+++
@@ -1,8 +0,0 @@
-
-<?php
- $output = exec("python -c 'import os;print(os.getcwd())'");
- //$output = exec("python3 /farmiq_master/web_predict1.py");
- 
- $variables = json_decode($output, true);
- var_dump($variables);
-
